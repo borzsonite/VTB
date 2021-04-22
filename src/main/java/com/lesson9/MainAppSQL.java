9a5f9cb947546cd3eda9ae02497456be2a031bff
Sergey Rozhkov <borzsonite@gmail.com>
2021-04-22 17:03:34 +0300

    01.15.54

--- src/main/java/com/lesson9/MainAppSQL.java
+++ src/main/java/com/lesson9/MainAppSQL.java
@@ -1,6 +1,8 @@
 package com.lesson9;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MainAppSQL {
@@ -8,13 +10,34 @@ public class MainAppSQL {
     private static Statement statement;
 
     public static void main(String[] args) {
+        try {
+            connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static void connect() {
+    public static void connect() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException e) {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            Statement statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new SQLException("Can't connect...");
+        }
+    }
+
+    public static void disconnect() {
+        try {
+            statement.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            connection.close();
+        } catch(SQLException e) {
             e.printStackTrace();
         }
     }
