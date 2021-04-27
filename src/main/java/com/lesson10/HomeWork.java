0a9e2429cfc4d4b9a369fa85efa5dace79425e38
Sergey <borzsonite@gmail.com>
2021-04-27 07:07:29 +0300

    HW start

---
+++ src/main/java/com/lesson10/HomeWork.java
@@ -0,0 +1,21 @@
+package com.lesson10;
+/**
+● Создайте таблицу студенты (students): id, имя, серия паспорта, номер паспорта;
+        1 Создайте таблицу Предметы (subjects): id, название предмета;
+        2 Создайте таблицу Успеваемость (progress): id, студент, предмет, оценка;
+        3 Оценка может находиться в пределах от 2 до 5;
+        4 Вывести список студентов, сдавших определенный предмет, на оценку выше 3;
+        5 При удалении студента из таблицы, вся его успеваемость тоже должна быть удалена;
+        6 Посчитать средний бал по определенному предмету;
+        7 Посчитать средний балл по определенному студенту;
+        8 Пара серия-номер пасорта должны быть уникальны в таблице Студенты;
+        9 Найти три премета, которые сдали наибольшее количество студентов;
+ */
+
+public class HomeWork {
+    // CREATE TABLE students2 (id serial, name text, passport_serial text, passport_number int);
+    // CREATE TABLE subjects2 (id serial, subject_name);
+    // CREATE TABLE progress2 (id serial, student text, subject text, score int ADD CHECK (score > 2 AND score < 6));
+    // SELECT name FROM students2 WHERE score > 3;
+
+}
